c40d6fc3f24dbd5876393ba82259a21f111f194a
mmosei <mmanly-osei@spartaglobal.com>
2018-06-05 12:03:03 +0100

    categories arrays set up

--- js/app.js
+++ js/app.js
@@ -2,4 +2,23 @@ $(document).ready(function(){
 
   console.log('working');
 
+var movieQuotes = [
+  'Frankly, my dear, I don\'t give a damn.',
+  'I\'m going to make him an offer he can\'t refuse.',
+  'Of all the gin joints in all the towns in all the world, she walks into mine.',
+  "You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya punk?"
+];
+var famousQuotes = [
+  "We are not makers of history. We are made by history.",
+  "Do not let your hearts be troubled. Trust in God\; trust also in me.",
+  "Happiness is when what you think, what you say, and what you do are in harmony.",
+  "Dress shabbily and they remember the dress; dress impeccably and they remember the woman."
+];
+var bookTitles = [
+  "The Wind In The Willows", "To Kill a Mockingbird", "Alice's Adventures in wonderland", "Of Mice and Men"
+];
+var surpriseMe = [movieQuotes,famousQuotes,bookTitles];
+
+var categories = [movieQuotes,famousQuotes,bookTitles,surpriseMe]];
+
 });
